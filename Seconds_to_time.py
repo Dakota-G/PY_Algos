b7391d5839b185bd77b8b2e6812f7f655be8499a
Dakota <dakota.giltner@gmail.com>
2020-04-02 23:43:26 -0700

    added a couple algo solutions

---
+++ Seconds_to_time.py
@@ -0,0 +1,11 @@
+# Found on codewars.com
+# Write a function, which takes a non-negative integer (seconds) as input
+# and returns the time in a human-readable format (HH:MM:SS)
+
+def make_readable(seconds):
+    ss = seconds%60
+    mm = (seconds-ss)/60
+    hh = ("{:02d}".format(int(mm/60)))
+    mm = ("{:02d}".format(int(mm%60)))
+    ss = ("{:02d}".format(int(ss)))
+    return(f"{hh}:{mm}:{ss}")
